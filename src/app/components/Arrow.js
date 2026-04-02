0fc9cb250231fda7f40e71ae0836648132cddf6a
Danila <waterfall.4415@gmail.com>
2026-04-02 19:34:29 +0300

    исправил баг с отображением checkbox

--- src/app/components/Arrow.js
+++ src/app/components/Arrow.js
@@ -2,7 +2,7 @@ import arrow from "@/app/assets/icons/arrow.svg";
 
 export default function Arrow ({  onChange, error  }) {
     return (
-        <img alt="arrow" className={`w-15 h-15 left-[-70px] top-[35px] absolute size-7 ... -scale-x-100 animate-arrow`}
+        <img alt="arrow" className={`xxs:hidden lg:block w-15 h-15 left-[-70px] top-[35px] absolute size-7 ... -scale-x-100 animate-arrow`}
              src={arrow.src}
         ></img>
     )
--- src/app/components/Checkbox.js
+++ src/app/components/Checkbox.js
@@ -1,11 +1,10 @@
 'use client';
-
 import React, { useState } from 'react';
 import Arrow from "@/app/components/Arrow";
-export default function Checkbox ({  onChange, error  }) {
+import checkbox from '@/app/assets/icons/Checkbox.svg'
+
+export default function Checkbox ({ onChange, error }) {
     const [isChecked, setIsChecked] = useState(false);
-
-
 
     const handleOnChange = (e) => {
         const checked = e.target.checked;
@@ -14,57 +13,49 @@ export default function Checkbox ({  onChange, error  }) {
     };
 
     return (
-        <div className={`relative`}>
-            {error ? <Arrow/> : ""}
+        <div className="relative">
+            {error ? <Arrow/> : null}
 
+            <label className={`
+                flex
+                items-center
+                gap-2
+                cursor-pointer
+                xxs:mt-4
+                xs:mt-3
+                sm:mt-[30px]
+                ${error ? "border-red-500 animate-shake" : "border-gray-400"}
+            `}>
+                <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={handleOnChange}
+                    className="hidden peer"
+                />
 
-            <div>
-                <label className={`
-                    flex
-                    items-center
-                    gap-2
-                    cursor-pointer
-                    xxs:mt-4
-                    xs:mt-3
-                    sm:mt-[30px]
-                    ${error ? "border-red-500 animate-shake" : "border-gray-400"}
-                `}>
-                    <input
-                        type="checkbox"
-                        checked={isChecked}
-                        onChange={handleOnChange}
-                        className="hidden peer"
-                    />
+                <div
+                    style={{
+                        backgroundImage: isChecked ? `url(${checkbox.src})` : 'none'
+                    }}
+                    className={`
+                        xxs:w-[30px] xxs:h-[30px]
+                        md:w-[32px] md:h-[32px]
+                        shrink-0
+                        border
+                        ${error ? "border-red-500 animate-error" : "border-gray-400"}
+                        rounded
+                        flex items-center justify-center
+                        bg-no-repeat
+                        bg-center
+                        hover:border-gray-200
+                        transition-all duration-200
+                    `}
+                />
 
-                    <div className={`
-                            md:w-[32px] md:h-[32px]
-                            xxs:w-[30px] xxs:h-[30px]
-                            border border-gray-400
-                            ${error ? "border-red-500 animate-error" : "border-gray-400"}
-                            rounded
-                            flex items-center justify-center
-                            peer-checked:bg-[url('@/app/assets/icons/Checkbox.svg')]
-                            bg-no-repeat
-                            bg-center
-                            hover:border-gray-200
-                            transition-all duration-200
-                    `}>
-                        <svg
-                            className="hidden peer-checked:block xxs:w-[19.09px] xxs:h-[13.54px] md:w-[20.36px] md:h-[14.55px]"
-                            viewBox="0 0 21 15"
-                            fill="none"
-                        >
-                            <path
-                                d="M19.4727..."
-                                fill="white"
-                            />
-                        </svg>
-                    </div>
-                    <span className="text-[#CDCDCD] xxs:text-[10px] xs:text-[12px] md:text-[16px]">
-                        Я согласен с <u>офертой рекуррентных платежей</u> и <u>Политикой конфиденциальности</u>
-                    </span>
-                </label>
-            </div>
+                <span className="text-[#CDCDCD] xxs:text-[10px] xs:text-[12px] md:text-[16px]">
+                    Я согласен с <u>офертой рекуррентных платежей</u> и <u>Политикой конфиденциальности</u>
+                </span>
+            </label>
         </div>
     );
 }
